469b2251514b1fec86b494bc835e7cde4bbcd2fd
redhat-debug <https://github.com/redhat-debug/ishq.git>
2022-06-03 02:41:04 +0600

    2

---
+++ assets/js/custom.js
@@ -0,0 +1,11 @@
+{
+   var swiper = new Swiper(".brandSwiper", {
+      freeMode: true,
+      autoplay: true,
+      spaceBetween: 15,
+      slidesPerView: 6,
+      centeredSlides: false,
+      loop: true,
+      speed: 1500,
+   });
+}
