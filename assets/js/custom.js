e9f83da689def3ca80a83f183ed34ace479186fb
redhat-debug <https://github.com/redhat-debug/ishq.git>
2022-06-03 14:48:05 +0600

    up

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -2,7 +2,7 @@
    var swiper = new Swiper(".brandSwiper", {
       freeMode: true,
       autoplay: true,
-      spaceBetween: 15,
+      spaceBetween: 30,
       slidesPerView: 6,
       centeredSlides: false,
       loop: true,
@@ -13,11 +13,10 @@
 {
    var swiper = new Swiper(".testimonialSwiper", {
       slidesPerView: "auto",
-      freeMode: true,
       centeredSlides: true,
       spaceBetween: 30,
       autoplay: true,
-      speed: 2000,
+      speed: 1500,
       loop: true,
       pagination: {
         el: ".swiper-pagination",
