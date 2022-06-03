e40b8827c54818412804d07f75683723bfda0e7b
redhat-debug <https://github.com/redhat-debug/ishq.git>
2022-06-03 18:05:06 +0600

    almost done

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -2,25 +2,64 @@
    var swiper = new Swiper(".brandSwiper", {
       freeMode: true,
       autoplay: true,
-      spaceBetween: 30,
       slidesPerView: 6,
       centeredSlides: false,
       loop: true,
       speed: 1500,
+      breakpoints: {
+         // when window width is >= 320px
+         320: {
+           slidesPerView: 1,
+           spaceBetween: 50,
+         },
+         // when window width is >= 480px
+         480: {
+           slidesPerView: 2,
+           spaceBetween: 15
+         },
+         // when window width is >= 640px
+         640: {
+           slidesPerView: 3,
+           spaceBetween: 30
+         },
+         // when window width is >= 991px
+         991: {
+           slidesPerView: 4,
+           spaceBetween: 30
+         },
+         // when window width is >= 1400px
+         1400: {
+           slidesPerView: 6,
+           spaceBetween: 30
+         }
+      }
    });
 }
 
 {
-   var swiper = new Swiper(".testimonialSwiper", {
-      slidesPerView: "auto",
-      centeredSlides: true,
-      spaceBetween: 30,
-      autoplay: true,
-      speed: 1500,
-      loop: true,
-      pagination: {
-        el: ".swiper-pagination",
-        clickable: true,
+  var swiper = new Swiper(".testimonialSwiper", {
+    slidesPerView: "auto",
+    centeredSlides: true,
+    spaceBetween: 30,
+    autoplay: true,
+    speed: 1500,
+    loop: true,
+    pagination: {
+      el: ".swiper-pagination",
+      clickable: true,
+    },
+
+    breakpoints: {
+      // when window width is >= 320px
+      320: {
+        slidesPerView: 1,
+        spaceBetween: 50,
       },
-   });
+      // when window width is >= 480px
+      680: {
+        slidesPerView: "auto",
+        spaceBetween: 15,
+      }
+    }
+  });
 }
