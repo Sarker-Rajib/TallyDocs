5b04f0f86cb4f49bcd7b02bd3ea6bf57fa331baa
redhat-debug <https://github.com/redhat-debug/ishq.git>
2022-06-04 02:24:49 +0600

    done

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -1,39 +1,35 @@
 {
-   var swiper = new Swiper(".brandSwiper", {
-      freeMode: true,
-      autoplay: true,
+  var swiper = new Swiper(".brandSwiper", {
+    freeMode: true,
+    autoplay: true,
+    slidesPerView: 6,
+    centeredSlides: true,
+    loop: true,
+    speed: 1500,
+    breakpoints: {
+      // when window width is >= 320px
+      320: {
+      slidesPerView: 2,
+      spaceBetween: 50,
+      },
+
+      // when window width is >= 640px
+      640: {
+      slidesPerView: 3,
+      spaceBetween: 50
+      },
+      // when window width is >= 991px
+      991: {
+      slidesPerView: 4,
+      spaceBetween: 50
+      },
+      // when window width is >= 1400px
+      1400: {
       slidesPerView: 6,
-      centeredSlides: false,
-      loop: true,
-      speed: 1500,
-      breakpoints: {
-         // when window width is >= 320px
-         320: {
-           slidesPerView: 1,
-           spaceBetween: 50,
-         },
-         // when window width is >= 480px
-         480: {
-           slidesPerView: 2,
-           spaceBetween: 15
-         },
-         // when window width is >= 640px
-         640: {
-           slidesPerView: 3,
-           spaceBetween: 30
-         },
-         // when window width is >= 991px
-         991: {
-           slidesPerView: 4,
-           spaceBetween: 30
-         },
-         // when window width is >= 1400px
-         1400: {
-           slidesPerView: 6,
-           spaceBetween: 30
-         }
+      spaceBetween: 60
       }
-   });
+    }
+  });
 }
 
 {
