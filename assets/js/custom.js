7bd6ddae50a330e7ae18a762705e03c1b095bb2e
redhat-debug <https://github.com/redhat-debug/ishq.git>
2022-06-03 14:35:34 +0600

    content written

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -9,3 +9,19 @@
       speed: 1500,
    });
 }
+
+{
+   var swiper = new Swiper(".testimonialSwiper", {
+      slidesPerView: "auto",
+      freeMode: true,
+      centeredSlides: true,
+      spaceBetween: 30,
+      autoplay: true,
+      speed: 2000,
+      loop: true,
+      pagination: {
+        el: ".swiper-pagination",
+        clickable: true,
+      },
+   });
+}
